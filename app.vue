aab48d3f90bb84f9ee5e4dab92a37d1b357b90ee
litiantaos <litiantaos@gmail.com>
2025-05-11 22:30:49 +0800

    feat: 增加 Vercel 分析和速度洞察组件

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
   <NuxtPage />
+  <Analytics />
+  <SpeedInsights />
 </template>
+
+<script setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+</script>
